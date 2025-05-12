3c4a0fc33113e26d6cf7628904b0d7861147f54c
wmacall <macallwalter@gmail.com>
2025-05-12 15:56:03 -0600

    ✨ Add new project button functionality and create new document page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import { ProjectRow } from "@/components/ProjectRow";
 import { Button } from "@/components/ui/button";
 import { ListFilterIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const { push } = useRouter();
+
+  const handleNewProject = () => push("/new-document");
+
   return (
     <div className="flex flex-col justify-center w-full">
       <div className="flex py-3.5 px-4 justify-between">
@@ -12,7 +18,10 @@ export default function Dashboard() {
             Sorted by <span className="text-black">Latest Edited</span>
           </h1>
         </div>
-        <Button className="bg-blue-600 hover:bg-blue-800 cursor-pointer p-0 h-[28px] px-2.5 gap-0.5">
+        <Button
+          onClick={handleNewProject}
+          className="bg-blue-600 hover:bg-blue-800 cursor-pointer p-0 h-[28px] px-2.5 gap-0.5"
+        >
           <Plus size={16} color="#ffffff" />
           <span className="text-sm font-bold">New Project</span>
         </Button>
