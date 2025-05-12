9d327e92ffe125d8f269e43e25db5daf2f33a3d8
wmacall <macallwalter@gmail.com>
2025-05-12 15:10:53 -0600

    ✨ Add project row

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,27 @@
+import { ProjectRow } from "@/components/ProjectRow";
+
 export default function Dashboard() {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <p className="mt-4 text-lg">Welcome to the dashboard!</p>
+      <div className="bg-neutral-50 w-full px-4 py-[5.5px]">
+        <h1 className="text-sm font-bold text-neutral-700">Today</h1>
+      </div>
+      <ProjectRow
+        title="Leveraging Technology for Efficient Communication"
+        date="March 25, 2024"
+      />
+      <div className="bg-neutral-50 w-full px-4 py-[5.5px]">
+        <h1 className="text-sm font-bold text-neutral-700">Yesterday</h1>
+      </div>
+      <ProjectRow title="The Investor Spectrum" date="March 25, 2024" />
+      <ProjectRow
+        title="Building Trust and Transparency"
+        date="March 25, 2024"
+      />
+      <ProjectRow
+        title="Engaging Investors Through Storytelling"
+        date="March 25, 2024"
+      />
     </div>
   );
 }
---
+++ src/components/ProjectRow/index.tsx
@@ -0,0 +1,37 @@
+import { MoreHorizontal, FileText } from "lucide-react";
+import { FC } from "react";
+
+interface ProjectRowProps {
+  title?: string;
+  date?: string;
+}
+
+export const ProjectRow: FC<ProjectRowProps> = ({ title = "", date = "" }) => {
+  return (
+    <div className="flex items-center justify-between w-full p-4 border-b border-gray-300/30 cursor-pointer">
+      <div className="flex items-center gap-2 flex-3">
+        <div className="h-8 w-8 items-center justify-center rounded-lg bg-white flex border border-gray-300/30">
+          <FileText className="w-4 h-4 text-gray-500" />
+        </div>
+        <span className="text-black font-medium text-sm">{title}</span>
+      </div>
+      <div className="flex items-center">
+        <div className="flex items-center bg-white border border-gray-300 rounded-full px-1.5 h-5 gap-1">
+          <span className="h-1 w-1 rounded-full bg-orange-500"></span>
+          <span className="text-gray-700 text-sm font-medium">
+            Investor Updates
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center flex-1 justify-end">
+        <span className="text-gray-500">{date}</span>
+      </div>
+      <div className="flex items-center flex-1 justify-end">
+        <span className="text-gray-500">{date}</span>
+      </div>
+      <div className="flex items-center flex-[0.5] justify-end">
+        <MoreHorizontal className="w-5 h-5 text-gray-500" />
+      </div>
+    </div>
+  );
+};
