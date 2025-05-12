c57799f8eeb5515b74be1ea5a83866f1692dba2b
wmacall <macallwalter@gmail.com>
2025-05-12 08:38:39 -0600

    ✨ Initial project setup, add auth form, add form input for reusability

---
+++ src/components/AuthForm/index.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import FormInput from "@/components/FormInput";
+import { Mail, Lock, Eye } from "lucide-react";
+
+interface AuthFormProps {
+  isSignIn?: boolean;
+  email: string;
+  setEmail: (email: string) => void;
+  password: string;
+  setPassword: (password: string) => void;
+}
+
+export default function AuthForm({
+  isSignIn = true,
+  email,
+  setEmail,
+  password,
+  setPassword,
+}: AuthFormProps) {
+  return (
+    <Card className="w-full max-w-md shadow-sm p-6">
+      <CardHeader className="text-center gap-4">
+        <CardTitle className="text-2xl font-bold">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </CardTitle>
+        <CardDescription>A few steps to access Redraft</CardDescription>
+      </CardHeader>
+      <CardContent className="px-0">
+        <div className="flex flex-col gap-3">
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={setEmail}
+            leftIcon={<Mail size={18} />}
+          />
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={setPassword}
+            leftIcon={<Lock size={18} />}
+            rightIcon={<Eye size={18} />}
+          />
+        </div>
+        {isSignIn && (
+          <h1 className="text-right py-1.5 font-medium mt-1.5 hover:underline cursor-pointer">
+            Forgot Password
+          </h1>
+        )}
+        <Button
+          className={cn(
+            "w-full bg-blue-500 hover:bg-blue-600 cursor-pointer",
+            isSignIn ? "mt-3" : "mt-6"
+          )}
+        >
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </Button>
+        <div className="flex items-center gap-2 justify-center pt-6">
+          <h1 className="text-gray-600">
+            {isSignIn ? "Don't have an account?" : "Already have an account?"}
+          </h1>
+          <Button variant="link" className="font-semibold cursor-pointer p-0">
+            {isSignIn ? "Sign Up" : "Sign In"}
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,68 @@
+import { Input } from "@/components/ui/input";
+import { Eye, EyeOff } from "lucide-react";
+import {
+  ChangeEvent,
+  HTMLInputTypeAttribute,
+  ReactNode,
+  useState,
+} from "react";
+
+interface FormInputProps {
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+  type?: HTMLInputTypeAttribute;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+}
+
+export default function FormInput({
+  name,
+  placeholder,
+  value,
+  onChange,
+  type = "text",
+  leftIcon,
+  rightIcon,
+}: FormInputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) =>
+    onChange(e.target.value);
+  const handleTogglePassword = () => setShowPassword(!showPassword);
+
+  const isPassword = type === "password";
+
+  return (
+    <div className="relative">
+      {leftIcon && (
+        <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none text-gray-400">
+          {leftIcon}
+        </div>
+      )}
+      <Input
+        name={name}
+        type={isPassword && showPassword ? "text" : type}
+        placeholder={placeholder}
+        className="pl-10 ring-0 focus:ring-0 focus-visible:ring-0"
+        value={value}
+        onChange={handleChangeValue}
+      />
+      {isPassword ? (
+        <div
+          className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-400"
+          onClick={handleTogglePassword}
+        >
+          {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+        </div>
+      ) : (
+        rightIcon && (
+          <div className="absolute inset-y-0 right-3 flex items-center text-gray-400">
+            {rightIcon}
+          </div>
+        )
+      )}
+    </div>
+  );
+}
